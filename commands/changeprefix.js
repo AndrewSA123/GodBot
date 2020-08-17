73417aec5e9619e5d01782b17288b620a0298753
AndrewSA123 <Andrewknott_123@hotmail.co.uk>
2020-08-17 16:38:49 +0100

    Change Prefix Command added

---
+++ commands/changeprefix.js
@@ -0,0 +1,22 @@
+module.exports = {
+    name: 'changeprefix',
+    description: 'Test Description',
+    execute(message, args){
+        const fs = require('fs');
+
+        let settings = JSON.parse(fs.readFileSync('Settings.json', 'utf8'));
+
+        if(args.length > 1){
+            if(args[1].length > 1){
+                message.reply("Can only be 1 character")
+            }else{
+                settings.Global.Prefix = args[1];
+                message.reply(`Prefix Changed To ${settings.Global.Prefix}`);
+                fs.writeFile('Settings.json', JSON.stringify(settings), function(err, result){});
+            }
+            
+        }else{
+            message.channel.send("Missing Parameter on|off");
+        }
+    }
+}
