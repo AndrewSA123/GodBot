c26a955b1b22139b28c69110689f79b057920aff
AndrewSA123 <Andrewknott_123@hotmail.co.uk>
2020-08-18 21:09:34 +0100

    Listen changes

--- commands/listen.js
+++ commands/listen.js
@@ -5,17 +5,16 @@ module.exports = {
         let settings = JSON.parse(fs.readFileSync('Settings.json', 'utf8'));
 
         const channel = client.channels.cache.get(settings.Global.DefaultVoice);
-        if (args.length > 1) {
-            if (args[1] === 'on') {
-                channel.join();
-                message.channel.send("Listening");
-                settings.Global.Listen = true;
-            } else {
-                channel.leave();
-                message.channel.send("Giving Back Privacy");
-                settings.Global.Listen = false;
-            }
-            fs.writeFile('Settings.json', JSON.stringify(settings), function (err, result) { });
+        if (!settings.Global.Listen) {
+            channel.join();
+            message.channel.send("Listening");
+            settings.Global.Listen = true;
+        } else {
+            channel.leave();
+            message.channel.send("Giving Back Privacy");
+            settings.Global.Listen = false;
         }
+        
+        fs.writeFile('Settings.json', JSON.stringify(settings), function (err, result) { });
     }
 }
