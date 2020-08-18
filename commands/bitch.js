794e9fe81378a31751ce54868b347514c3e65172
AndrewSA123 <Andrewknott_123@hotmail.co.uk>
2020-08-18 22:48:26 +0100

    Bitch Mode updated

--- commands/bitch.js
+++ commands/bitch.js
@@ -4,18 +4,12 @@ module.exports = {
     execute(message, args, client, fs){
         let settings = JSON.parse(fs.readFileSync('Settings.json', 'utf8'));
 
-        if(args.length > 1){
-            if(args[1].toLowerCase() == 'on'){
-                settings.Global.Bitch = true;
-                message.channel.send("Bitch Mode Activated");
-            }else if(args[1].toLowerCase() == 'off'){
-                settings.Global.Bitch = false;
-                message.channel.send("Bitch Mode Off");
-            }else{
-                message.channel.send("Invalid Parameter on|off");
-            }
+        if(settings.Global.Bitch){
+            settings.Global.Bitch = false;
+            message.channel.send("Bitch Mode Deactivated")
         }else{
-            message.channel.send("Missing Parameter on|off");
+            settings.Global.Bitch = true;
+            message.channel.send("Bitch Mode Activated");
         }
 
         fs.writeFile('Settings.json', JSON.stringify(settings), function(err, result){});
