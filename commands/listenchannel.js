e4f721833dd6d85176f304af84861aa55a633c5e
AndrewSA123 <Andrewknott_123@hotmail.co.uk>
2020-08-18 01:11:04 +0100

    ListenChannel and ListenTarget commands added

---
+++ commands/listenchannel.js
@@ -0,0 +1,14 @@
+module.exports = {
+    name: 'listenchannel',
+    description: 'Test Description',
+    execute(message, args, client, fs){
+
+        let settings = JSON.parse(fs.readFileSync('Settings.json', 'utf8'));
+
+        if(args.length > 1){
+            settings.Global.DefaultVoice = args[1];
+            fs.writeFile('Settings.json', JSON.stringify(settings), function (err, result) { });
+        }
+
+    }
+}
