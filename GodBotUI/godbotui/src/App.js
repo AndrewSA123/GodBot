22dc8f72a323dd3fb5bde01976f676763ee843db
AndrewSA123 <Andrewknott_123@hotmail.co.uk>
2020-09-13 15:28:50 +0100

    React front end init

--- GodBotUI/godbotui/src/App.js
+++ GodBotUI/godbotui/src/App.js
@@ -8,15 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Spinny thing ^
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/AndrewSA123/GodBot"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          GodBot Github
         </a>
       </header>
     </div>
